8e738fbd8c8ed2b7182574a3a2ab11cd1755d34d
Sitthata <aprils96950@gmail.com>
2024-10-12 16:59:10 +0700

    Refactor closure counter function

--- src/advance-js/closure/counter.js
+++ src/advance-js/closure/counter.js
@@ -1,25 +1,27 @@
 function createCounter() {
-    let counter = 0;
+  let counter = 0;
 
-    function increment() {
-        counter += 1;
-    }
+  function increment() {
+    counter += 1;
+  }
 
-    function decrement() {
-        counter -= 1;
-    }
+  function decrement() {
+    counter -= 1;
+  }
 
-    function getCount() {
-        return counter;
-    }
+  function getCount() {
+    return counter;
+  }
 
-    return { increment, decrement, getCount };
+  return { increment, decrement, getCount };
 }
 
 const counter = createCounter();
 
-counter.increment();
-counter.increment();
-counter.increment();
+console.log(typeof counter.increment) 
 
-console.log(counter.getCount());
+counter.increment()
+counter.increment()
+counter.increment()
+
+console.log(counter.getCount())
